b7c9854a8967dfd3921fe8df14ac5566061e57e8
Alfredo Vanegas <thealf154@gmail.com>
2023-05-08 11:10:53 -0600

    DEV: Working prototype

--- make_df.py
+++ make_df.py
@@ -6,6 +6,7 @@ import concurrent.futures
 import threading
 from bs4 import BeautifulSoup
 import os
+import csv
 
 thread_local = threading.local()
 
@@ -27,9 +28,9 @@ TICKERS = [
 ]
 
 OUTPUT_PATH = os.path.join('./results/', 'output' + '.csv')
+OUTPUT_PATH2 = os.path.join('./results/', 'output2' + '.csv')
 
-def get_news_df(ticker_url, ticker):
-    news_dict = []
+def get_news_df(ticker_url, ticker, writer):
     r = requests.get(ticker_url)
     root = ET.fromstring(r.text)
     for item in root.iter('item'):
@@ -37,44 +38,55 @@ def get_news_df(ticker_url, ticker):
         link = item.find('link').text
         author = item.find('author').text
         author = author.replace('(', '').replace(')', '')
-        description, pub_date = get_accurate_description(link)
-        news = {
-            'ticker': ticker,
-            'title': title, 
-            'description': description,
-            'pub_date':pub_date,
-            'link':link,
-            'author':author
-        }
-        news_dict.append(news)
-    pd.DataFrame(data=news_dict).to_csv(OUTPUT_PATH, mode='a', index=False)
+        description = item.find('description').text
+        pub_date = item.find('pubDate').text
+        news = [ ticker, title, description, pub_date, link, author ]
+        writer.writerow(news)
+        print(f"SUCCESSFUL NEWS EXTRACTION [{ticker}]: {link}")
 
 # This method is a little slow for parsing the web
 # but since there's embedded code in the paragraphs, svg, and other elements,
 # it would be a nightmare to parse trough a custom method
-def get_accurate_description(link):
+def get_accurate_description(row, writer):
     description = ''
-    pub_date = ''
-    r = requests.get(link, headers=HEADERS)
+    r = requests.get(row['link'], headers=HEADERS)
     html = r.text
     soup = BeautifulSoup(html, 'lxml')
     content = soup.find('div', class_='caas-body')
     if content:
-        pub_date = soup.find('time').string if soup.find('time') else ''
         for p in content.find_all('p'):
             p_text = p.string if p.string != None else ''
             description += p_text
-        description = description
-    return description, pub_date
+        row['pub_date'] = soup.find('time').string if soup.find('time') else ''
+        row['description'] = description
+    print(f"SUCCESSFUL DESCRIPTION EXCTRACTION [{row['ticker']}]: {row['link']}")
+    writer.writerow(row.to_list())
+
+def write_header(writer):
+    header = ['ticker', 'title', 'description', 'pub_date', 'link', 'author'] 
+    writer.writerow(header)
 
 def get_news_driver():
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for ticker in TICKERS:
-            ticker_url = FINVIZ + ticker
-            executor.submit(get_news_df, ticker_url, ticker)
+    with open(OUTPUT_PATH, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        write_header(writer)
+        with concurrent.futures.ThreadPoolExecutor(4) as executor:
+            for ticker in TICKERS:
+                ticker_url = FINVIZ + ticker
+                executor.submit(get_news_df, ticker_url, ticker, writer)
+
+def get_description_driver():
+    df = pd.read_csv(OUTPUT_PATH)
+    with open(OUTPUT_PATH2, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        write_header(writer)
+        with concurrent.futures.ThreadPoolExecutor(4) as executor:
+            for _, row in df.iterrows():
+                executor.submit(get_accurate_description, row, writer)
 
 def main():
     get_news_driver()
+    get_description_driver()
 
 if __name__ == '__main__':
     main()
--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-text = ""
-with open('./test.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-content = re.search(r"<div class=\"caas-body\">.*</div>", text)
-print(content)
